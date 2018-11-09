ed12165356511afbd4ab2019f22718325abed6f4
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-11-09 08:47:07 +0000

    fix healthcheck script

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+set +e
+
 PROJECT_DIR="${HOME}/go/src/github.com/ethereum/go-ethereum"
 
-if [ ! -S /vagrant/node/tomo.ipc ]; then
-    if ! ${PROJECT_DIR}/build/bin/tomo attach /vagrant/node/tomo.ipc --exec 'eth.blockNumber'; then
-        echo "Node stopped! Restart it!"
-        cd /vagrant && nohup bash ./node.sh<&- &>/vagrant/node.log &
-    fi
+${PROJECT_DIR}/build/bin/tomo attach /vagrant/node/tomo.ipc --exec 'eth.blockNumber'
+retval=$?
+
+if [ $retval -ne 0 ]; then
+    echo "Node stopped! Restart it!"
+    cd /vagrant && nohup bash ./node.sh<&- &>/vagrant/node.log &
 fi
