7bead71dd985e076ea0d630e14ded489c8ebea1e
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-11-09 08:01:49 +0000

    add sshKeys

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+PROJECT_DIR="${HOME}/go/src/github.com/ethereum/go-ethereum"
+
 if [ ! -S /vagrant/node/tomo.ipc ]; then
-    echo "Node stopped! Restart it!"
-    cd /vagrant && nohup bash ./node.sh<&- &>/vagrant/node.log &
+    if ! ${PROJECT_DIR}/build/bin/tomo attach /vagrant/node/tomo.ipc --exec 'eth.blockNumber'; then
+        echo "Node stopped! Restart it!"
+        cd /vagrant && nohup bash ./node.sh<&- &>/vagrant/node.log &
+    fi
 fi
