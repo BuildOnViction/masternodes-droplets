03a41fe826a8a613aef67f97327008a6866be899
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-11-10 03:58:52 +0000

    healthcheck for main node

--- healthcheck.sh
+++ healthcheck.sh
@@ -3,11 +3,12 @@
 set +e
 
 PROJECT_DIR="${HOME}/go/src/github.com/ethereum/go-ethereum"
+DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
 
 ${PROJECT_DIR}/build/bin/tomo attach /vagrant/node/tomo.ipc --exec 'eth.blockNumber'
 retval=$?
 
 if [ $retval -ne 0 ]; then
     echo "Node stopped! Restart it!"
-    cd /vagrant && nohup bash ./node.sh<&- &>/vagrant/node.log &
+    cd /vagrant && nohup bash ./node.sh<&- &>/vagrant/node.${DATE_WITH_TIME}.log &
 fi
