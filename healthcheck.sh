31f5a4381925fac63e5622bf9d239e95182aa4f7
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-11-20 04:40:42 +0000

    add websocket, healtcheck

--- healthcheck.sh
+++ healthcheck.sh
@@ -11,4 +11,22 @@ retval=$?
 if [ $retval -ne 0 ]; then
     echo "Node stopped! Restart it!"
     cd /vagrant && nohup bash ./node.sh<&- &>/vagrant/node.${DATE_WITH_TIME}.log &
+    exit 0
 fi
+
+blockNumber=`${PROJECT_DIR}/build/bin/tomo attach /vagrant/node/tomo.ipc --exec 'eth.blockNumber'`
+
+if [ ! -f /tmp/.healthcheck ]; then
+    echo ${blockNumber} > /tmp/.healthcheck
+    exit 0
+fi
+
+prvBlockNumber=`cat /tmp/.healthcheck`
+
+if [ "${blockNumber}" == "${prvBlockNumber}" ]; then
+    # restart node
+    echo "Node does not work! Restart it!"
+    cd /vagrant && nohup bash ./node.sh<&- &>/vagrant/node.${DATE_WITH_TIME}.log &
+fi
+echo ${blockNumber} > /tmp/.healthcheck
+
--- main.sh
+++ main.sh
@@ -33,7 +33,8 @@ echo Starting the nodes ...
 ${PROJECT_DIR}/build/bin/tomo \
     --bootnodes "enode://7d8ffe6d28f738d8b7c32f11fb6daa6204abae990a842025b0a969aabdda702aca95a821746332c2e618a92736538761b1660aa9defb099bc46b16db28992bc9@127.0.0.1:30301" \
     --syncmode "full" --datadir ./node --networkid ${NETWORK_ID} --port 30303 \
-    --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8545 --ws --wsaddr 0.0.0.0 \
+    --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8545 \
+    --ws --wsaddr 0.0.0.0 \
     --wsport 8546 --wsorigins "*" --maxpeers 25 \
     --rpcvhosts "*" \
     --unlock "${wallet}" \
