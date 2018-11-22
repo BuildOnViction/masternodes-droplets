531e188491282098fb2740512d5c8f0860e295fd
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-11-22 11:10:45 +0000

    change params config

--- node.sh
+++ node.sh
@@ -18,7 +18,7 @@ GASPRICE="2500"
 echo Starting the node ...
 ${PROJECT_DIR}/build/bin/tomo --bootnodes "enode://7d8ffe6d28f738d8b7c32f11fb6daa6204abae990a842025b0a969aabdda702aca95a821746332c2e618a92736538761b1660aa9defb099bc46b16db28992bc9@${MAIN_IP}:30301" \
 	--syncmode 'full' \
-    --maxpeers 25 \
+    --maxpeers 200 \
     --txpool.globalqueue 5000 \
     --txpool.globalslots 5000 \
 	--datadir ./node --networkid ${NETWORK_ID} --port 30303 --rpc --rpccorsdomain "*" \
