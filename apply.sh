6d52cd7f954063ea2861d0f799c90e67d19a8b19
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-11-23 04:45:12 +0000

    remove check before apply

--- apply.sh
+++ apply.sh
@@ -9,16 +9,7 @@ echo $chainBlockNumber
 echo $nodeBlockNumber
  
 if [ "${chainBlockNumber}" == "${nodeBlockNumber}" ]; then
-    # store the whole response with the status at the and
-    HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X GET ${TOMOMASTER}/api/candidates/${COINBASE_ADDRESS}/isCandidate)
-
-    # extract the body
-    HTTP_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
-
-
-    if [ "${HTTP_BODY}" != "1" ]; then
-        curl --header "Content-Type: application/json" \
-          --request POST \
-          "${TOMOMASTER}/api/candidates/apply?key=${OWNER_PRIVATE_KEY}&coinbase=${COINBASE_ADDRESS}&name=${NODE_NAME}"
-    fi
+    curl --header "Content-Type: application/json" \
+      --request POST \
+      "${TOMOMASTER}/api/candidates/apply?key=${OWNER_PRIVATE_KEY}&coinbase=${COINBASE_ADDRESS}&name=${NODE_NAME}"
 fi
