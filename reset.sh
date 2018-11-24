3e72abf28c8ea064d842a87a698b322a9472bdda
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-11-24 04:10:56 +0000

    add consul, consul watch to update code

--- reset.sh
+++ reset.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
 PROJECT_DIR="${HOME}/go/src/github.com/ethereum/go-ethereum"
-cd ${PROJECT_DIR} && git pull origin master && make all
+DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
+cd ${PROJECT_DIR} 
+
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse @{u})
+BASE=$(git merge-base @ @{u})
+
+echo Local $LOCAL Base $BASE Remote $REMOTE
+if [ $LOCAL != $REMOTE ] && [ $LOCAL == $BASE ]; then
+    echo "Stop node!"
+    pkill -f "bin/tomo"
+    git pull origin master && make all
+    echo "Restart it!"
+    cd /vagrant && nohup bash ./node.sh<&- &>/vagrant/node.reset.${DATE_WITH_TIME}.log &
+else
+    echo "Nothing to do!!!"
+fi
